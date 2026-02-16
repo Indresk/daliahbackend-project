7f45979d60a87178b1cf6822923a8036ba827356
Indresk <gif.poto@gmail.com>
2026-02-16 15:13:07 -0500

    re vinculación de webhook

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ import express from 'express'
 import cors from 'cors'
 import AuthRouter from './routes/auth.router.js'
 import { apiLimiter } from './middlewares/rateLimiters.js'
+import WebhookRouter from './routes/webhooks.router.js'
+
+const PORT = process.env.PORT
 
 const app = express()
 
-app.use(cors({ origin: 'https://daliahbanda.com', credentials: true }))
+// app.use(cors({ origin: 'https://daliahbanda.com', credentials: true }))
 app.use(express.json())
-
 app.set('trust proxy', 1)
-app.use('/auth', AuthRouter)
 
+app.use('/auth', AuthRouter)
+app.use('/webhook', WebhookRouter)
 app.use('/api', apiLimiter)
 
-app.listen(3023, () => {
-    console.log(`Servidor levantado en puerto ${3023}`)
+app.listen(PORT, () => {
+    console.log(`Servidor levantado en puerto ${PORT}`)
 })
---
+++ src/webhooks/kick/getSubscription.js
@@ -0,0 +1,39 @@
+export async function getStatus(payload) {
+  if (!payload) return;
+  
+  const eventType = `${payload.event}.${payload.type}`;
+  console.log('Evento Kick:', eventType, payload);
+  
+  if (eventType === 'livestream.status.updated') {
+    const isLive = payload.data?.is_live ?? false;
+    
+    console.log(isLive ? '🔴 Stream INICIADO' : '⏹️ Stream TERMINADO');
+  }
+}
+
+export async function getLiveStatus() {
+  try {
+    return false;
+  } catch (error) {
+    console.error('Error:', error.message);
+    return false;
+  }
+}
+
+export async function verifySubscriptions(accessToken) {
+  const response = await fetch(
+    'https://api.kick.com/public/v1/events/subscriptions',
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  )
+
+  if (!response.ok) {
+    const error = await response.text()
+    throw new Error(`Get subscription error: ${error}`)
+  }
+
+  return response.json()
+}
