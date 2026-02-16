14b13fe131d395fb8e21b546f1a653826573fdf2
Indresk <gif.poto@gmail.com>
2026-02-16 16:34:40 -0500

    ajuste de middleware

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use(cors({ origin: 'https://daliahbanda.com', credentials: true }))
-app.use(express.json())
 app.set('trust proxy', 1)
 
 app.use('/auth', AuthRouter)
