febb5e138422be3d99e2c9a35a96b712aec3676c
Indresk <gif.poto@gmail.com>
2026-02-17 00:30:36 -0500

    ajuste de importación de fb

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import { addRefreshToken, removeRefreshToken, hasRefreshToken } from './tokenStore.js'
 import { verifyPassword } from './hashing.js'
+import { getUser } from '../db/firebase.js'
 
 const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET
 const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET
